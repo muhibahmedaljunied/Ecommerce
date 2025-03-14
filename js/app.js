6cc6953d1abeb45c5345b84d44475a997559bc05
unknown <muhibalmuhib10@gmail.com>
2025-03-14 03:39:37 +0300

    deploy on docker

--- js/app.js
+++ js/app.js
@@ -72435,7 +72435,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\Ecommerce\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/resources/js/app.js */"./resources/js/app.js");
 
 
 /***/ })
